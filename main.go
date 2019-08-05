2befcfe0ecefd9a3296308a8abe60f71dd554eb3
Iman Tumorang <iman.tumorang@gmail.com>
2019-08-05 23:47:39 +0700

    feat(structs): add custom type for function

--- main.go
+++ main.go
@@ -9,12 +9,19 @@ import (
 func main() {
 	deretanKata := model.DeretanKata{"Ora Et Labora", "Que Sera Sera"}
 	deretanKata.Cetak()
+
+	// Create model.Introduction
+	selfIntro := model.Introduction(func(s model.Student) string {
+		return "Hello, My name is " + string(s.Name) + " and my class is " + s.Class
+	})
+
 	stdntSnow := model.Student{
-		Name:   model.CustomString("John Snow"),
-		Class:  "D3A",
-		Quotes: deretanKata,
+		Name:          model.CustomString("John Snow"),
+		Class:         "D3A",
+		Quotes:        deretanKata,
+		SelfIntroduce: selfIntro,
 	}
-	fmt.Println(stdntSnow)
+	fmt.Println(stdntSnow.IntroduceSelf())
 	siswa := model.Siswa(stdntSnow)
 	fmt.Println(siswa)
 }
--- model/student.go
+++ model/student.go
@@ -4,9 +4,10 @@ import "fmt"
 
 // Student ...
 type Student struct {
-	Name   CustomString
-	Class  string
-	Quotes DeretanKata
+	Name          CustomString
+	Class         string
+	Quotes        DeretanKata
+	SelfIntroduce Introduction
 }
 
 // CustomString  must set to public (first letter must capitalized) so it can used accross projects
@@ -20,3 +21,11 @@ func (d DeretanKata) Cetak() {
 		fmt.Printf("\"%s\"\n", item)
 	}
 }
+
+// IntroduceSelf is a Student function
+func (s Student) IntroduceSelf() string {
+	return s.SelfIntroduce(s)
+}
+
+// Introduction is a custom type of a function.
+type Introduction func(s Student) string
